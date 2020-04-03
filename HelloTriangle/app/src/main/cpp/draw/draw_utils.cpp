262199fd775349e7e6aafd1899d55a9b10c89fac
Wang Chenguang(Chauncy) <wcg3031@nj.apac.arcsoft.corp>
2020-04-03 09:53:25 +0800

    draw triangle by GL_ELEMENT_ARRAY_BUFFER

--- HelloTriangle/app/src/main/cpp/draw/draw_utils.cpp
+++ HelloTriangle/app/src/main/cpp/draw/draw_utils.cpp
@@ -7,6 +7,7 @@
 #include "shader_content.h"
 #include "Shader_Helper.h"
 #include "MyDefine.h"
+#include "Utils.h"
 
 int drawTriangle (Shader_Helper *pShaderHelper)
 {
@@ -14,9 +15,9 @@ int drawTriangle (Shader_Helper *pShaderHelper)
 	CHECK_NULL_INPUT(pShaderHelper)
 
 	float vertex[]{
-			0.5f, 0, 0,
-			0, 0.5f, 0,
-			-0.5f, 0, 0
+		0.5f, 0, 0,
+		0, 0.5f, 0,
+		-0.5f, 0, 0
 	};
 
 	GLuint VAO, VBO;
@@ -29,14 +30,47 @@ int drawTriangle (Shader_Helper *pShaderHelper)
 	glEnableVertexAttribArray(0);
 	glBindVertexArray(0);
 
-	pShaderHelper->use();
+	float vertex_multi[]{
+		0.75, -0.75f, 0,
+		0, -0.5f, 0,
+		-0.5f, -0.1f, 0,
+		0.2, -0.3, 0
+	};
+	int index_multi[]{
+		0, 1, 2,
+		1, 2, 3
+	};
+	GLuint VAO2, VBO2, EBO;
+	glGenVertexArrays(1, &VAO2);
+	glBindVertexArray(VAO2);
+	glGenBuffers(1, &VBO2);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_multi), vertex_multi, GL_STATIC_DRAW);
 
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_multi), index_multi, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+
+	pShaderHelper->use();
 	glBindVertexArray(VAO);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glBindVertexArray(0);
 
+	pShaderHelper->setfloat("g_color", 1.0f);
+	glBindVertexArray(VAO2);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
 	glDeleteBuffers(1, &VBO);
 	glDeleteVertexArrays(1, &VAO);
+
+	glDeleteBuffers(1, &EBO);
+	glDeleteBuffers(1, &VBO2);
+	glDeleteVertexArrays(1, &VAO2);
 
 	return 0;
 }
--- HelloTriangle/app/src/main/cpp/draw/shader_content.h
+++ HelloTriangle/app/src/main/cpp/draw/shader_content.h
@@ -11,19 +11,21 @@ using namespace std;
 
 static const string triangle_vertex_shader = "#version 300 es\n"
 									 "layout (location = 0) in vec3 aPos;\n"
-									 "\n"
+									 "out float g_color = 0.1f;\n"
 									 "void main()\n"
 									 "{\n"
 									 "    gl_Position = vec4(aPos, 1.0);\n"
+		  							 "	  g_color = "
 									 "}";
 
 static const string triangle_fragment_shader = "#version 300 es\n"
 											   "precision mediump float;\n"
+			  								   "in float g_color;"
 											   "out vec4 FragColor;\n"
 											   "\n"
 											   "void main()\n"
 											   "{\n"
-											   "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+											   "    FragColor = vec4(1.0f, g_color, 0.2f, 1.0f);\n"
 											   "}";
 
 #endif //HELLOTRIANGLE_SHADER_CONTENT_H
