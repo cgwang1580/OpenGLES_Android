865f764c149e6db4451d7b74c87c7f0c89f8ec4d
cgwang1580 <cgwang1580@gmail.com>
2020-05-26 10:15:27 +0800

    fix bug of SampleTransform.cpp

--- SeniorOpenGLES/app/src/main/cpp/draw/SampleTransform.cpp
+++ SeniorOpenGLES/app/src/main/cpp/draw/SampleTransform.cpp
@@ -7,8 +7,7 @@
 #include <MyDefineUtils.h>
 #include <GLES3/gl3.h>
 #include "SampleTransform.h"
-
-#define CHECK_OK(_ret_)		if (ERROR_OK != _ret_) {return _ret_;}
+#include "DrawHelper.h"
 
 const GLfloat vVertex[24] {
 		-0.5f,  0.5f,  0.5f,
@@ -18,7 +17,7 @@ const GLfloat vVertex[24] {
 		-0.5f, -0.5f,  0.5f,
 		0.5f, -0.5f, -0.5f,
 		0.5f, -0.5f, -0.5f,
-		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f
 };
 
 /*static const GLfloat vTexture[] {
@@ -43,11 +42,24 @@ const GLuint vIndex[36] {
 SampleTransform::SampleTransform()
 {
 	LOGD("SampleTransform::SampleTransform");
+	m_VAO = GL_NONE;
+	m_VBO = GL_NONE;
+	m_EBO = GL_NONE;
+	for (auto val : m_sVBO)
+		val = GL_NONE;
 }
 
 SampleTransform::~SampleTransform()
 {
 	LOGD("~SampleTransform::SampleTransform");
+	SafeDeleteGLBuffer (1, &m_VAO);
+	SafeDeleteGLBuffer (1, &m_VBO);
+	SafeDeleteGLBuffer (1, &m_EBO);
+	SafeDeleteGLBuffer (2, m_sVBO);
+	m_VAO = m_VBO = m_EBO = GL_NONE;
+	for (auto val : m_sVBO)
+		val = GL_NONE;
+	SafeDelete(m_pShaderHelper);
 }
 
 RESULT SampleTransform::createShader()
@@ -63,18 +75,24 @@ RESULT SampleTransform::creteGLBuffer ()
 {
 	LOGD("SampleTransform::creteGLBuffer");
 
+	glGenVertexArrays(1, &m_VAO);
+	glBindVertexArray(m_VAO);
 	glGenBuffers(2, m_sVBO);
 	glBindBuffer(GL_ARRAY_BUFFER, m_sVBO[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vVertex), vVertex, GL_STATIC_DRAW);
+	DrawHelper::CheckGLError("creteGLBuffer glBufferData");
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_sVBO[1]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vIndex), vIndex, GL_STATIC_DRAW);
+	DrawHelper::CheckGLError("creteGLBuffer glBufferData");
 
-	glGenBuffers(1, &m_VAO);
-	glBindVertexArray(m_VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, m_sVBO[0]);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+	DrawHelper::CheckGLError("creteGLBuffer glBindBuffer");
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+	DrawHelper::CheckGLError("creteGLBuffer glVertexAttribPointer");
+	glBindBuffer(GL_ARRAY_BUFFER, GL_NONE);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_sVBO[1]);
 	glBindVertexArray(GL_NONE);
 
 	return ERROR_OK;
@@ -85,17 +103,21 @@ RESULT SampleTransform::InitSample ()
 	LOGD("SampleTransform::InitSample");
 	int ret = ERROR_OK;
 	createShader();
-	CHECK_OK(ret);
-	creteGLBuffer();
-	CHECK_OK(ret);
+	CHECK_OK_RETURN(ret)
+	ret = creteGLBuffer();
+	CHECK_OK_RETURN(ret);
 	return ret;
 }
 
 void SampleTransform::UnInitSample ()
 {
 	LOGD("SampleTransform::UnInitSample");
-	if (m_VAO) glDeleteBuffers(1, &m_VAO);
-	glDeleteBuffers(2, m_sVBO);
+	SafeDeleteGLBuffer (1, &m_VAO);
+	SafeDeleteGLBuffer (1, &m_VBO);
+	SafeDeleteGLBuffer (1, &m_EBO);
+	SafeDeleteGLBuffer (2, m_sVBO);
+	m_VAO = m_VBO = m_EBO = GL_NONE;
+	for (auto  val : m_sVBO) val = GL_NONE;
 	SafeDelete (m_pShaderHelper);
 }
 
@@ -103,8 +125,12 @@ RESULT SampleTransform::OnDrawFrame ()
 {
 	LOGD("SampleTransform::onDrawFrame");
 	m_pShaderHelper->use();
+	DrawHelper::CheckGLError("OnDrawFrame use");
 	glBindVertexArray(m_VAO);
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, (void*)0);
+	DrawHelper::CheckGLError("OnDrawFrame glBindVertexArray");
+	glDrawElements(GL_TRIANGLES, sizeof(vIndex)/ sizeof(GLuint), GL_UNSIGNED_INT, (void*)0);
+	DrawHelper::CheckGLError("OnDrawFrame glDrawElements");
 	glBindVertexArray(GL_NONE);
+	DrawHelper::CheckGLError("OnDrawFrame glBindVertexArray");
 	return ERROR_OK;
 }
--- SeniorOpenGLES/app/src/main/cpp/draw/SampleTransform.h
+++ SeniorOpenGLES/app/src/main/cpp/draw/SampleTransform.h
@@ -41,7 +41,7 @@ private:
 											"\n"
 											"void main()\n"
 											"{\n"
-											"    FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);\n"
+											"    FragColor = vec4(1.0f, 0.0f, 0.2f, 1.0f);\n"
 											"}";
 };
 
